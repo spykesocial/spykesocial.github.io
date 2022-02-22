b6f4658ec5bdfcb28b381ccd3333612f2152304b
yashdiniz <yashdiniz@gmail.com>
2022-02-22 12:23:08 +0530

    Added custom image optimiser

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   images: {
     domains: ['assets.vercel.com'],
-    formats: ['image/avif', 'image/webp'],
+    formats: ['image/avif', 'image/webp', 'image/png'],
+    loader: 'akamai',
+    path: '/',
   },
 }
