056dab1a51400580f1edafb2acb6d30d98663a93
yashdiniz <yashdiniz@gmail.com>
2022-02-23 12:40:30 +0530

    Added NextSEO!

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,34 @@
 import Logo from '../components/Logo';
 
+// using next-seo to add a title and meta, for better Search Engine results.
+// Also helps add cards/banners for messages on WhatsApp or Twitter.
+import { NextSeo } from 'next-seo';
+
 export default function Home() {
   return (
     <>
+      <NextSeo 
+        title="Spyke 🗿 Coming Soon"
+        description="Spyke Social ‒ Find your Klan!⁂"
+        openGraph={{
+            url: 'https://spy.ke',
+            title: 'Spyke 🗿 Coming Soon',
+            description: 'Spyke Social ‒ Find your Klan!⁂',
+            images: [
+                { 
+                    url: 'https://spy.ke/spyke.png',
+                    alt: 'Spyke Social Private Limited',
+                    type: 'image/png',
+                }
+            ],
+            site_name: 'Spyke 🗿 Home'
+        }}
+        twitter={{
+            handle: '@spykesocial',
+        }}
+      />
+
+      { /* body of page starts here... */ }
       <Logo/>
     </>
   );
