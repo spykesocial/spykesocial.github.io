7ba542708353d973b8df4380b9066a9cdefbb756
yashdiniz <yashdiniz@gmail.com>
2022-02-23 23:41:09 +0530

    Latest Export

--- docs/_next/static/ZR2V2J_zcfwXJ4G0gBqhO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/130-09161b30efb9a398.js","static/chunks/pages/index-a21b262de143ec62.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ docs/_next/static/skBO7gzoQvHCCL2U1C9V5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c06729fd5376c5c8.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
