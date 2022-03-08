083450428625a1452cee428d29cda4da9e76fe49
yashdiniz <yashdiniz@gmail.com>
2022-03-08 07:41:13 +0000

    Deploying to gh-pages from @ spykesocial/spykesocial.github.io@3eaefc41f11d1aa049e615d58aef1465f231d050 🚀

---
+++ _next/static/VUKAPZdUa9luzw8ek8u8p/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c73b49a310d4b625.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
