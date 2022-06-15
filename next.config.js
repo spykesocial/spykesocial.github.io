3cb8a41357bc21a826b4a7a0246096c14a3d6433
Eeshan Keni <eeshan.keni@gmail.com>
2022-06-15 23:08:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-
-  experimental: {
-    headers() {
-      return [
-        {
-          source: "/.well-known/apple-app-site-association",
-          headers: [{ key: "content-type", value: "application/json" }]
-        }
-      ];
-    }
-  },
-
   images: {
     domains: ['assets.vercel.com'],
     formats: ['image/avif', 'image/webp'],
